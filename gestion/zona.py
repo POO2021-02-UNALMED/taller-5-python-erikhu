c6c89e216bf5668686a3225604110b78f9ce4762
Erik Gonzalez <erikhuboy@gmail.com>
2021-11-23 23:24:08 -0500

    zoo and zona

--- gestion/zona.py
+++ gestion/zona.py
@@ -1,2 +1,17 @@
 class Zona:
-    pass
+    def __init__(self, nombre, zoo):
+        self._nombre = nombre
+        self._zoo = zoo
+        self._animales = []
+
+    def getZoo(self):
+        return self._zoo
+
+    def getNombre(self):
+        return self._nombre
+
+    def agregarAnimales(self, animal):
+        self._animales.insert(animal)
+
+    def cantidadAnimales(self):
+        return self._animales.count()
---
+++ gestion/zoologico.py
@@ -0,0 +1,20 @@
+class Zoologico:
+    def __init__(self, nombre, ubicacion):
+        self._nombre = nombre
+        self._ubicacion = ubicacion
+        self._zonas = []
+
+    def getNombre(self):
+        return self._nombre
+
+    def getUbicacion(self):
+        return self._ubicacion
+
+    def agregarZonas(self, zona):
+        self._zonas.insert(zona)
+
+    def cantitadTotalAnimales(self):
+        contador = 0
+        for zona in  self._zonas:
+            contador = contador + zona.cantidadAnimales()
+        return contador
