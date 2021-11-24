3a3ec682f806d6af694cc7ce0017a187f08bf933
Erik Gonzalez <erikhuboy@gmail.com>
2021-11-23 23:38:27 -0500

    test

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,8 +1,8 @@
-from zooAnimales.anfibio import Anfibio
-from zooAnimales.mamifero import Mamifero
-from zooAnimales.ave import Ave
-from zooAnimales.reptil import Reptil
-from zooAnimales.pez import Pez
+# from zooAnimales.anfibio import Anfibio
+# from zooAnimales.mamifero import Mamifero
+# from zooAnimales.ave import Ave
+# from zooAnimales.reptil import Reptil
+# from zooAnimales.pez import Pez
 
 class Animal:
     totalAnimales = 0;
@@ -37,7 +37,8 @@ class Animal:
 
     @classmethod
     def totalPorTipo(cls):
-        return "Mamiferos: {}\nAves: {}\nReptiles: {}\nPeces: {}\nAnfibios: {}".format(Mamifero.cantidadMamiferos(), Ave.cantidadAves(), Reptil.cantidadReptiles(), Pez.cantidadPeces(), Anfibio.cantidadAnfibios())
+        #return "Mamiferos: {}\nAves: {}\nReptiles: {}\nPeces: {}\nAnfibios: {}".format(Mamifero.cantidadMamiferos(), Ave.cantidadAves(), Reptil.cantidadReptiles(), Pez.cantidadPeces(), Anfibio.cantidadAnfibios())
+        return ""
 
     def toString(self):
         return "Mi nombre es {}, tengo una edad de #edad,habito en {} y mi genero es {}".format(self._nombre, self._edad, self._habitat, self._genero)
