01a64f6fa77a3db7519b789eec20d26cc58fd416
Erik Gonzalez <erikhuboy@gmail.com>
2021-11-23 23:16:06 -0500

    update cantidad por tipo

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,3 +1,9 @@
+from zooAnimales.anfibio import Anfibio
+from zooAnimales.mamifero import Mamifero
+from zooAnimales.ave import Ave
+from zooAnimales.reptil import Reptil
+from zooAnimales.pez import Pez
+
 class Animal:
     totalAnimales = 0;
 
@@ -31,7 +37,7 @@ class Animal:
 
     @classmethod
     def totalPorTipo(cls):
-        return "Mamiferos: {}\nAves: {}\nReptiles: {}\nPeces: {}\nAnfibios: {}".format()
+        return "Mamiferos: {}\nAves: {}\nReptiles: {}\nPeces: {}\nAnfibios: {}".format(Mamifero.cantidadMamiferos(), Ave.cantidadAves(), Reptil.cantidadReptiles(), Pez.cantidadPeces(), Anfibio.cantidadAnfibios())
 
     def toString(self):
         return "Mi nombre es {}, tengo una edad de #edad,habito en {} y mi genero es {}".format(self._nombre, self._edad, self._habitat, self._genero)
