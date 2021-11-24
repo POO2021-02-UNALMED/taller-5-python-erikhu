1a5cf7c73384c0dbcff0378fcce11603810974f5
Erik Gonzalez <erikhuboy@gmail.com>
2021-11-24 00:04:53 -0500

    fix

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -38,7 +38,7 @@ class Animal:
         from zooAnimales.ave import Ave
         from zooAnimales.reptil import Reptil
         from zooAnimales.pez import Pez
-        return "Mamiferos: {}\nAves: {}\nReptiles: {}\nPeces: {}\nAnfibios: {}".format(Mamifero.cantidadMamiferos(), Ave.cantidadAves(), Reptil.cantidadReptiles(), Pez.cantidadPeces(), Anfibio.cantidadAnfibios())
+        return "Mamiferos: {}\nAves: {}\nReptiles: {}\nPeces: {}\nAnfibios: {}".format([Mamifero.cantidadMamiferos(), Ave.cantidadAves(), Reptil.cantidadReptiles(), Pez.cantidadPeces(), Anfibio.cantidadAnfibios()])
 
     def toString(self):
         return "Mi nombre es {}, tengo una edad de {}, habito en {} y mi genero es {}".format(self._nombre, self._edad, self._habitat, self._genero)
